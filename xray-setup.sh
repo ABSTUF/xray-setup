682144125e60ec614581ab32401050e78d5bbae8
ABSTUF <shipin90@mail.ru>
2026-09-21 19:42:30 +0300

    Switch SNI/dest to ya.ru

--- xray-setup.sh
+++ xray-setup.sh
@@ -16,7 +16,7 @@ if [ -z "$PRIV" ] || [ -z "$PUB" ]; then
 fi
 echo "--- Check: PRIV=${PRIV:0:8}... PUB=${PUB:0:8}... (must not be empty) ---"
 SID=$(openssl rand -hex 8)
-SNI=www.microsoft.com
+SNI=ya.ru
 
 echo "=== 3/6 Writing config ==="
 cat > config.json <<XEOF
